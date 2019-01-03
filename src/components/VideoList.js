e31fd1ce1bd7403845eaf97e00d738cecb151fc2
BrianArchibald <brianallenarchibald@gmail.com>
2019-01-02 20:01:01 -0600

    vid now plays, some styling, setting title, descr of vids

--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import VideoItem from './VideoItem';
 
-const VideoList = ({ videos }) => {  // destructure ({ videos })
+const VideoList = ({ videos, onVideoSelect }) => {  // destructure ({ videos })
 	const renderedList = videos.map((video) => {
-		return <VideoItem video={video} />;
+		return <VideoItem key={video.id.videoId} onVideoSelect={onVideoSelect} video={video} />;
 	});
 
 	return <div className="ui relaxed divided list" >{renderedList}</div>;
